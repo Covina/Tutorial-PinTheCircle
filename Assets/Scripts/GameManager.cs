3a84f008b84c120a507dd1768c727d7512f101a9
Covina <jabs303@gmail.com>
2017-08-23 08:25:50 -0700

    Created gamemanager framework

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
+	// for creating a singleton
+	public static GameManager instance;
+
+
+	[SerializeField] private int howManyNeedles;
+
+	// get access to the needle object
+	[SerializeField] private GameObject needle;
+
+
+	// the shoot button
+	private Button shootButton;
+
+	// store the needles to be fired and in play for this round
+	private GameObject[] needles;
+
+	// 
+	private float needleDistance = 1.5f;
+
+	//
+	private int needleIndex;
+
+
 	// Use this for initialization
-	void Start () {
+	void Awake () {
 		
 	}
 	
